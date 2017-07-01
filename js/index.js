ab69ea069ce22b9cbaeafff616d3d3777892758d
Fernando Boza <hi@fernandoboza.com>
2017-07-01 05:37:28 -0400

    update jss

--- js/index.js
+++ js/index.js
@@ -14,28 +14,24 @@ document.addEventListener('DOMContentLoaded', function(){
         stringsElement: document.getElementById('typed-strings-1'),
         typeSpeed: 30,
         startDelay: 0,
-        // callback: function(){ foo(); },
     });
     
     Typed.new("#cmd-2", {
         stringsElement: document.getElementById('typed-strings-2'),
         typeSpeed: 130,
         startDelay: 2400,
-        // callback: function(){ foo(); },
     });
     
     Typed.new("#cmd-3", {
         stringsElement: document.getElementById('typed-strings-3'),
         typeSpeed: 130,
         startDelay: 4500,
-        // callback: function(){ foo(); },
     });
     
     Typed.new("#cmd-4", {
         stringsElement: document.getElementById('typed-strings-4'),
         typeSpeed: 130,
         startDelay: 5500,
-        // callback: function(){ foo(); },
     });
 });
 
